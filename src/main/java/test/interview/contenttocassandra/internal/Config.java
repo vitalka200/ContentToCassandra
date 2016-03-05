c120d21ef5ad699053087e6c79b4ee278201739b
vitalka200 <vitalka200@users.noreply.github.com>
2016-03-05 08:32:36 +0200

    Changed default server to be '127.0.0.1'

--- src/main/java/test/interview/contenttocassandra/internal/Config.java
+++ src/main/java/test/interview/contenttocassandra/internal/Config.java
@@ -1,7 +1,7 @@
 package test.interview.contenttocassandra.internal;
 
 public class Config {
-	public static final String SERVER_NAME = "tlv-it-c7-1";
+	public static final String SERVER_NAME = "127.0.0.1";
 	public static final String CAS_KEYSPACE = "test";
 	public static final int MAX_CHUNK_SIZE = 10*1024; 
 
